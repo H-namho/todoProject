cebefa7545bf7e4f60ff881f21eb7074d282d389
H-namho <skagh9696@gmail.com>
2026-05-21 10:29:58 +0900

    main

---
+++ src/main/java/com/example/memorypratice/MemoryPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.memorypratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryPraticeApplication.class, args);
+    }
+
+}
